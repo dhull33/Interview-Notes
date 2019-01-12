81abec0aa17ab41dd1e8ddd22d7b35111f721594
David Hull <david.hull37@gmail.com>
2019-01-11 21:45:29 -0600

    Added gitignore

---
+++ practice.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    i_exponent = len(str(n))
+    i = 10**i_exponent
+    j = 1
+    sum_total = n
+    while j < n:
+        sum_total = (sum_total + (n-j)*i)
+        j += 1
+        i_exponent = len(str(sum_total))
+        i = 10**i_exponent
+    print(sum_total)
