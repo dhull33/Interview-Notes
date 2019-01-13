b130d6bf37ce4ed1aa576f097ccef2d9af8a56c0
David Hull <david.hull37@gmail.com>
2019-01-13 15:28:08 -0600

    Added a slice up my string function and person class

--- practice.py
+++ practice.py
@@ -1,3 +1,16 @@
+"""
+Given a string S, of length N that is indexed from 0 to N-1, print its even
+indexed and odd-indexed characters as 2 space separated strings on a single
+line. 0 is considered an even index in this exercise.
+
+"""
+def slice_my_string():
+    test_cases = int(input('How many strings do you want to slice? '))
+    for i in range(test_cases):
+        my_string = input('Enter your string: ')
+        n = len(my_string)
+        print(my_string[0:n+1:2], my_string[1:n+1:2])
+
 if __name__ == '__main__':
     n = int(input())
     i_exponent = len(str(n))
