be5ade2a15fef2b58b1bea6150c6bf633af2316e
David Hull <david.hull37@gmail.com>
2019-01-15 15:54:07 -0600

    Added a reverse array of numbers problem

--- HackerRankProblems/practice.py
+++ HackerRankProblems/practice.py
@@ -1,10 +1,12 @@
-"""
-Given a string S, of length N that is indexed from 0 to N-1, print its even
-indexed and odd-indexed characters as 2 space separated strings on a single
-line. 0 is considered an even index in this exercise.
 
-"""
+
+
 def slice_my_string():
+    """
+    Given a string S, of length N that is indexed from 0 to N-1, print its even
+    indexed and odd-indexed characters as 2 space separated strings on a single
+    line. 0 is considered an even index in this exercise.
+    """
     test_cases = int(input('How many strings do you want to slice? '))
     for i in range(test_cases):
         my_string = input('Enter your string: ')
@@ -15,12 +17,12 @@ def slice_my_string():
         print(my_string[0:n+1:2], my_string[1:n+1:2])
 
 
-""" 
-Read an integer N. Without any string methods, print the following: 1234...N.
-Note that "..." represents the values in between. 
 
-"""
 def print_number_as_string():
+    """
+    Read an integer N. Without any string methods, print the following: 1234...N.
+    Note that "..." represents the values in between.
+    """
     n = int(input("Enter an integer: "))
     i_exponent = len(str(n))
     i = 10**i_exponent
@@ -32,3 +34,18 @@ def print_number_as_string():
         i_exponent = len(str(sum_total))
         i = 10**i_exponent
     print(sum_total)
+
+
+def reverse_array_of_numbers():
+    """
+    Given an array, A, of N integers, print A's elements in reverse order
+    as a single line of space separated numbers
+    """
+    given_array = list(map(int, input("Enter a space separated sequence of "
+                                      "numbers: ").rstrip().split()))
+
+    reverse_array = given_array[::-1]
+    for num in reverse_array:
+        print(num, end=" ")
+
+reverse_array_of_numbers()
