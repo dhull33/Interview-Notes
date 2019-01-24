fe7e626f6e832557d312589581b8c945e03f2801
David Hull <david.hull37@gmail.com>
2019-01-24 17:29:42 -0600

    Created a new linked list class

--- HackerRankProblems/30DaysOfCode.py
+++ HackerRankProblems/30DaysOfCode.py
@@ -134,36 +134,69 @@ def hourglass():
     print(max_sum)
 
 # Implementing a Linked List
-class Node:
-    def __init__(self, data):
-        self.data = data
+class Element:
+    def __init__(self, value):
+        self.value = value
         self.next = None
-class Solution:
-    def display(self, head):
-        current = head
-        while current:
-            print(current.data, end=' ')
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, new_element):
+        """
+        Adds a new element to the end of the list
+        """
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+    def get_position(self, position):
+        """Get an element from a particular position. Assume the first
+        position is "1". Return None if position is not in the list"""
+        counter = 1
+        current = self.head
+        if position < 1:
+            return None
+        while current and counter <= position:
+            if position == counter:
+                return current
+            counter += 1
             current = current.next
+        return None
 
-    def insert(self, head, data):
-        # if the list has no elements, we return a new node
-        new_node = Node(data)
-        if head is None:
-            return new_node
-        # otherwise we iterate through the list, add the node to the tail,
-        # and return head
-        temp = head
-        while temp.next is not None:
-            temp = temp.next
-        temp.next = Node(data)
-        return head
+    def insert(self, new_element, position):
+        """Insert a new node at the given position.
+        Assume the first position is "1".
+        Inserting at position 3 means between
+        the 2nd and 3rd elements."""
+        counter = 1
+        current = self.head
+        if position > 1:
+            while current and counter < position:
+                if counter == position - 1:
+                    new_element.next = current.next
+                    current.next = new_element
+                current = current.next
+                counter += 1
+        elif position == 1:
+            new_element.next = self.head
+            self.head = new_element
 
-# Creates a linked list by implementing the Node class
-def createLinkedList():
-    mylist = Solution()
-    T = int(input("Enter the length of linked list: "))
-    head = None
-    for i in range(T):
-        data = int(input("Enter an integer: "))
-        head = mylist.insert(head, data)
-    mylist.display(head)
+
+    def delete(self, value):
+        """Delete the first node with a given value."""
+        current = self.head
+        previous = None
+        while current.value != value and current.next:
+            previous = current
+            current = current.next
+        if current.value == value:
+            if previous:
+                previous.next = current.next
+            else:
+                self.head = current.next
